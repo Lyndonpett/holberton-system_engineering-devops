94ba145b2c5ba05fbc198858985ef416633214d9
lyndonpett <lyndonpett98@gmail.com>
2022-03-25 15:36:01 -0500

    Not something stupud

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
-'''Gathering data from api and exporting to CSV'''
-
+'''
+This script will gather and process data from a REST API
+ and process it for display
+'''
 import csv
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    '''
+    finds data from api
+    formats data
+    outputs data
+    '''
+    task_completed = []
+    # task_completed_count = 0
+    employee_id = int(sys.argv[1])
+    employees = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                             .format(employee_id))
+    employees_info = employees.json()['username']
+    # (alt way for above)employees_info = employees.get('name')
+    all_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(employee_id))
+    task_list = all_tasks.json()
 
-def saveTasksToCSV(employeeID):
-    '''Gathering data from api and exporting to CSV'''
+    for task in task_list:
+        task_detail = []
+        task_detail.append(employee_id)
+        task_detail.append(employees_info)
+        task_detail.append(str(task.get('completed')))
+        task_detail.append(task.get('title'))
+        task_completed.append(task_detail)
 
-    # variables
-    username = ''
-    allTasks = []
-
-    # requests
-    employeeReq = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employeeID))
-    todoReq = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            employeeID))
-
-    # get json from requests
-    username = employeeReq.json().get('username')
-    todoJSON = todoReq.json()
-
-    for task in todoJSON:
-        taskRow = []
-        taskRow.append(employeeID)
-        taskRow.append(username)
-        taskRow.append(task.get('completed'))
-        taskRow.append(task.get('title'))
-        allTasks.append(taskRow)
-
-    with open('{}.csv'.format(employeeID), 'w') as csvFile:
-        csvWriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
-        csvWriter.writerows(allTasks)
-
-    return 0
-
-
-if __name__ == '__main__':
-    saveTasksToCSV(argv[1])
+    with open('{}.csv'.format(employee_id), 'w', newline='')as f:
+        writer = csv.writer(f, dialect='unix')
+        writer.writerows(task_completed)
